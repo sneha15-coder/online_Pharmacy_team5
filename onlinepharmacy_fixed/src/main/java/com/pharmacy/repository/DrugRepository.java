b87d211aef110bda27a25c5783764a860bb71b18
Gundiah <gundiah7351@gmail.com>
2025-08-29 15:40:07 +0530

    Delete onlinepharmacy_fixed directory

--- onlinepharmacy_fixed/src/main/java/com/pharmacy/repository/DrugRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pharmacy.repository;
-
-import com.pharmacy.model.Drug;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DrugRepository extends JpaRepository<Drug, Integer> {
-}
